157951be80e4c59c8c4d55bb96966eda02e2aa3d
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-08-30 15:18:46 +0300

    Fix collisions

--- Assets/Scripts/Attractable.cs
+++ Assets/Scripts/Attractable.cs
@@ -9,12 +9,22 @@ public class Attractable : MonoBehaviour
 
 	[SerializeField] private float mass;
 	[SerializeField] private Vector3 velocity;
+	[SerializeField] private GameObject fragmentPrefab;
 
 	public float Mass => mass;
 
 	public void AddVelocity(Vector3 v)
 	{
 		velocity += v;
+	}
+
+	private void SpawnFragments(int numFragments, Vector3 forces)
+	{
+		for (int i = 0; i < numFragments; i++)
+		{
+			var newFragment = Instantiate(fragmentPrefab, transform.position, Quaternion.identity).GetComponent<Attractable>();
+			newFragment.AddVelocity(forces);
+		}
 	}
 
 	private void OnEnable()
@@ -30,5 +40,21 @@ public class Attractable : MonoBehaviour
 	private void Update()
 	{
 		transform.position += velocity * Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (fragmentPrefab != null)
+		{
+			Vector3 direction = (collision.transform.position - transform.position).normalized;
+			Vector3 surfaceNormal = collision.GetContact(0).normal;
+
+			Vector3 reflectionVector = Vector3.Reflect(direction, surfaceNormal);
+			reflectionVector = reflectionVector.normalized * 0.75f;
+
+			SpawnFragments(100, reflectionVector);
+		}
+
+		Destroy(gameObject);
 	}
 }
