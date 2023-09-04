fea28d8940171a47a320ecd3e9125a519a27b5a2
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-04 21:48:53 +0300

    Add moon orbital radius line

---
+++ Assets/Scripts/OrbitalRadiusRenderer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class OrbitalRadiusRenderer : MonoBehaviour
+{
+	[SerializeField] private GameObject parent;
+	[SerializeField] private GameObject child;
+	[SerializeField] private int numVertices = 200;
+
+	private LineRenderer lineRenderer;
+
+	private void Start()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.positionCount = numVertices + 1;
+
+		float radius = Vector3.Distance(parent.transform.position, child.transform.position);
+
+		Vector3[] positions = new Vector3[numVertices + 1];
+		for (int i = 0; i <= numVertices; i++)
+		{
+			float angle = (float)i / numVertices * 360.0f;
+			float x = radius * Mathf.Cos(Mathf.Deg2Rad * angle);
+			float y = radius * Mathf.Sin(Mathf.Deg2Rad * angle);
+			positions[i] = new Vector3(x, y, 0.0f);
+		}
+
+		lineRenderer.SetPositions(positions);
+	}
+}
