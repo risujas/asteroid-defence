e9907bf61e81fd646c1b6e334b919f330fe8c92c
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-05 02:53:43 +0300

    Add velocity modifier, heading to asteroid spawner

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
 {
+	[SerializeField] private Attractor centralBody;
 	[SerializeField] private float spawnDistance = 10.0f;
-	[SerializeField] private List<GameObject> asteroidPrefabs = new();
+	[SerializeField] private List<GameObject> asteroidPrefabs = new List<GameObject>();
 	[SerializeField] private float minScale = 0.1f;
 	[SerializeField] private float maxScale = 0.2f;
 
@@ -16,11 +17,27 @@ public class AsteroidSpawner : MonoBehaviour
 		var randomPrefab = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Count)];
 		var newAsteroid = Instantiate(randomPrefab, spawnPos, Quaternion.identity, transform);
 		newAsteroid.transform.localScale = Vector3.one * Random.Range(minScale, maxScale);
+
+		var attractable = newAsteroid.GetComponent<Attractable>();
+		DefineAsteroidOrbit(attractable, Random.Range(0.5f, 1.0f), Random.Range(-90.0f, 90.0f));
+	}
+
+	private void DefineAsteroidOrbit(Attractable attractable, float velocityModifier, float facing)
+	{
+		Vector3 vectorAB = centralBody.transform.position - attractable.transform.position;
+		float distance = vectorAB.magnitude;
+		vectorAB.Normalize();
+		Vector3 angledVector = Quaternion.Euler(0, 0, facing) * vectorAB;
+
+		float orbitalVelocity = Mathf.Sqrt(Attractor.G * centralBody.Mass / distance) * velocityModifier;
+		var velocityVector = angledVector * orbitalVelocity;
+
+		attractable.AddVelocity(velocityVector);
 	}
 
 	private void Start()
 	{
-		for (int i = 0; i < 20; i++)
+		for (int i = 0; i < 100; i++)
 		{
 			SpawnAsteroid();
 		}
