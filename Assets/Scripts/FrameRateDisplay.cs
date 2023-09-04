3a9f085a3b64ef4e5741ce65f9df3525b5bd5678
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-04 19:50:17 +0300

    Add FPS display

---
+++ Assets/Scripts/FrameRateDisplay.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FrameRateDisplay : MonoBehaviour
+{
+	private float updateInterval = 0.1f;
+	private float lastUpdate = 0.0f;
+
+	private List<float> fpsCounts = new();
+
+	[SerializeField] private TextMeshProUGUI textMesh;
+
+	private void Update()
+	{
+		if (Time.time > lastUpdate + updateInterval)
+		{
+			float currentFps = 1.0f / Time.deltaTime;
+			fpsCounts.Add(currentFps);
+
+			if (fpsCounts.Count > 10)
+			{
+				fpsCounts.RemoveAt(0);
+			}
+
+			float avgFps = 0.0f;
+			foreach (var i in fpsCounts)
+			{
+				avgFps += i;
+			}
+			avgFps /= fpsCounts.Count;
+
+			string text = "FPS: " + avgFps.ToString();
+			textMesh.text = text;
+
+			lastUpdate = Time.time;
+		}
+	}
+}
