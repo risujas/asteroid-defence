929371cfdf4c501b849d06af7eb6737a0f432200
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-06 02:07:09 +0300

    Update Attractable.cs

--- Assets/Scripts/Attractable.cs
+++ Assets/Scripts/Attractable.cs
@@ -43,15 +43,15 @@ public class Attractable : MonoBehaviour
 	{
 		if (vfxPrefab != null)
 		{
-			Vector3 impactPoint = collision.GetContact(0).point;
-			var vfx = Instantiate(vfxPrefab, impactPoint, Quaternion.identity, collision.gameObject.transform);
+			Vector3 spawnPoint = collision.GetContact(0).point;
+			var vfx = Instantiate(vfxPrefab, spawnPoint, Quaternion.identity, collision.gameObject.transform);
 			vfx.transform.up = collision.GetContact(0).normal;
 		}
 
 		if (impactLightPrefab != null)
 		{
-			Vector3 impactPoint = collision.GetContact(0).point + collision.GetContact(0).normal * 0.025f;
-			Instantiate(impactLightPrefab, impactPoint, Quaternion.identity, collision.gameObject.transform);
+			Vector3 spawnPoint = collision.GetContact(0).point + collision.GetContact(0).normal * 0.025f;
+			Instantiate(impactLightPrefab, spawnPoint, Quaternion.identity, collision.gameObject.transform);
 		}
 
 		if (fragmentPrefab != null)
@@ -66,8 +66,8 @@ public class Attractable : MonoBehaviour
 				individualVector = Quaternion.AngleAxis(Random.Range(-30.0f, 30.0f), Vector3.forward) * individualVector;
 
 				float width = transform.localScale.x * 0.5f;
-				Vector3 impactPoint = collision.GetContact(0).point + collision.GetContact(0).normal * 0.025f;
-				Vector3 spawnPosition = impactPoint + Random.insideUnitSphere * Random.Range(-width, width);
+				Vector3 spawnPoint = collision.GetContact(0).point + collision.GetContact(0).normal * 0.03f;
+				Vector3 spawnPosition = spawnPoint += Random.insideUnitSphere * Random.Range(-width, width);
 				spawnPosition.z = 0.0f;
 
 				var newFragment = Instantiate(fragmentPrefab, spawnPosition, Quaternion.identity, transform.parent).GetComponent<Attractable>();
