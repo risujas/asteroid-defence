dec90dc70e68429b302ec84c36876809ce1bbd40
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-09 20:08:09 +0300

    Update Temperature.cs

--- Assets/Temperature.cs
+++ Assets/Temperature.cs
@@ -14,8 +14,6 @@ public class Temperature : MonoBehaviour
 	void Update()
 	{
 		float summerness = Mathf.Abs(0.5f - solarSeason.YearProgress) * 2.0f;
-		Debug.Log(solarSeason.YearProgress + " - " + summerness);
-
 		float icecapExtent = Mathf.Lerp(0.4f, 0.1f, summerness);
 		material.SetFloat("_IceCapCoverage", icecapExtent);
 	}
