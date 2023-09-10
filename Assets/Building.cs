0be34059f3bcb079b7b4ecdbad8b268ff069cb8a
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-10 04:18:22 +0300

    wip

--- Assets/Building.cs
+++ Assets/Building.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float buildCost = 20.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float BuildCost => buildCost;
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+	}
 }
--- Assets/BuildingManager.cs
+++ Assets/BuildingManager.cs
@@ -5,7 +5,10 @@ public class BuildingManager : MonoBehaviour
 {
 	[SerializeField] private float spawnHeight = 0.5f;
 	[SerializeField] private List<Building> spawnableBuildings = new();
+	[SerializeField] private float buildPoints = 100.0f;
 
+	private bool isPlacingBuilding = false;
+	private Building placedBuildingPrefab = null;
 
 	public float SpawnHeight => spawnHeight;
 
@@ -15,5 +18,23 @@ public class BuildingManager : MonoBehaviour
 
 	private void Update()
 	{
+		if (placedBuildingPrefab == null)
+		{
+			placedBuildingPrefab = spawnableBuildings[0];
+		}
+
+		// NONE OF THIS WORKS
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			mouseWorldPos.z = 0.0f;
+
+			Vector3 spawnDir = (mouseWorldPos - transform.position).normalized;
+			Vector3 spawnPos = spawnDir * spawnHeight;
+
+			var newBuilding = Instantiate(placedBuildingPrefab, spawnPos, Quaternion.identity);
+			newBuilding.transform.parent = transform;
+		}
 	}
 }
