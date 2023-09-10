62765c4eb085e279e1a2b98729197f20b9c06834
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-10 15:47:20 +0300

    Add RadiusRenderer

--- Assets/Scripts/OrbitalRadiusRenderer.cs
+++ Assets/Scripts/OrbitalRadiusRenderer.cs
@@ -1,32 +1,15 @@
 using UnityEngine;
 
-public class OrbitalRadiusRenderer : MonoBehaviour
+public class OrbitalRadiusRenderer : RadiusRenderer
 {
 	[SerializeField] private GameObject parent;
 	[SerializeField] private GameObject child;
-	[SerializeField] private int numVertices = 200;
 
-	private LineRenderer lineRenderer;
-
-	private void Start()
+	protected override void Start()
 	{
 		transform.position = parent.transform.position;
+		radius = Vector3.Distance(parent.transform.position, child.transform.position);
 
-		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.useWorldSpace = false;
-		lineRenderer.positionCount = numVertices + 1;
-
-		float radius = Vector3.Distance(parent.transform.position, child.transform.position);
-
-		Vector3[] positions = new Vector3[numVertices + 1];
-		for (int i = 0; i <= numVertices; i++)
-		{
-			float angle = (float)i / numVertices * 360.0f;
-			float x = radius * Mathf.Cos(Mathf.Deg2Rad * angle);
-			float y = radius * Mathf.Sin(Mathf.Deg2Rad * angle);
-			positions[i] = new Vector3(x, y, 0.0f);
-		}
-
-		lineRenderer.SetPositions(positions);
+		base.Start();
 	}
 }
