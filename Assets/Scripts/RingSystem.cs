e5123daba97164e8ba1a4b79e43c5f889d0faed6
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-20 00:19:35 +0300

    Ring shader updates

--- Assets/Scripts/RingSystem.cs
+++ Assets/Scripts/RingSystem.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class RingSystem : MonoBehaviour
 {
-	[SerializeField] private RingSystem rearSideClone = null;
 	[SerializeField] private float innerRadius = 0.5f;
 	[SerializeField] private float outerRadius = 1.0f;
 	[SerializeField] private Color color;
@@ -25,28 +24,6 @@ public class RingSystem : MonoBehaviour
 		material.SetColor("_Emission", emission);
 		material.SetFloat("_Smoothness", smoothness);
 		material.SetFloat("_RotationSpeed", rotationSpeed);
-
-		if (rearSideClone != null)
-		{
-			if (rearSideClone.material == null)
-			{
-				rearSideClone.material = rearSideClone.GetComponent<Renderer>().sharedMaterial;
-			}
-
-			rearSideClone.innerRadius = innerRadius;
-			rearSideClone.outerRadius = outerRadius;
-			rearSideClone.color = color;
-			rearSideClone.emission = emission;
-			rearSideClone.smoothness = smoothness;
-			rearSideClone.rotationSpeed = rotationSpeed;
-
-			rearSideClone.material.SetFloat("_InnerRadius", innerRadius);
-			rearSideClone.material.SetFloat("_OuterRadius", outerRadius);
-			rearSideClone.material.SetColor("_Color", color);
-			rearSideClone.material.SetColor("_Emission", emission);
-			rearSideClone.material.SetFloat("_Smoothness", smoothness);
-			rearSideClone.material.SetFloat("_RotationSpeed", rotationSpeed);
-		}
 	}
 
 	private void OnValidate()
