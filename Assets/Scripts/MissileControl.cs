97b414e5c82e03f5d6f33e8d98d6345e1aaafd67
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-19 23:20:47 +0300

    Add delta-v to missile control

--- Assets/Scripts/MissileControl.cs
+++ Assets/Scripts/MissileControl.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MissileControl : MonoBehaviour
 {
+	[SerializeField] private float maxDeltaV = 10.0f;
+	[SerializeField] private float remainingDeltaV = 10.0f;
 	[SerializeField] private float acceleration = 1.0f;
 
 	private Attractable attractable;
@@ -19,10 +21,24 @@ public class MissileControl : MonoBehaviour
 
 		transform.up = direction.normalized;
 
+		if (remainingDeltaV > maxDeltaV)
+		{
+			remainingDeltaV = maxDeltaV;
+		}
+
 		if (Input.GetMouseButton(0))
 		{
-			Vector3 deltaV = transform.up * acceleration * Time.deltaTime;
-			attractable.AddVelocity(deltaV);
+			if (remainingDeltaV > 0.0f)
+			{
+				float deltaV = acceleration * Time.deltaTime;
+				if (remainingDeltaV < deltaV)
+				{
+					deltaV = remainingDeltaV;
+				}
+
+				remainingDeltaV -= deltaV;
+				attractable.AddVelocity(transform.up * deltaV);
+			}
 		}
 
 		if (Input.GetMouseButtonUp(1))
