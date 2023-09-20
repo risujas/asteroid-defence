96e3d1942a671f2556e4bc7211cdfbcf7a263fea
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-20 17:49:51 +0300

    Improve RingSystem material sharing

--- Assets/Scripts/RingSystem.cs
+++ Assets/Scripts/RingSystem.cs
@@ -2,28 +2,37 @@ using UnityEngine;
 
 public class RingSystem : MonoBehaviour
 {
+	[SerializeField] private Material defaultMaterial;
+	[SerializeField] private GameObject rearSide;
+
 	[SerializeField] private float innerRadius = 0.5f;
 	[SerializeField] private float outerRadius = 1.0f;
-	[SerializeField] private Color color;
-	[SerializeField] private Color emission;
-	[SerializeField] private float smoothness = 0.5f;
+	[SerializeField] private Color color = Color.white;
+	[SerializeField] private float smoothness = 0.66f;
 	[SerializeField] private float rotationSpeed = 0.0f;
+	[SerializeField] private float noiseScale = 7000.0f;
+	[SerializeField] private float noiseStrength = 0.3f;
 
-	private Material material;
+	private Material material = null;
 
 	private void SetMaterialAttributes()
 	{
-		if (material == null)
-		{
-			material = GetComponent<Renderer>().sharedMaterial;
-		}
+		material = Instantiate(defaultMaterial);
+		GetComponent<Renderer>().sharedMaterial = material;
+		rearSide.GetComponent<Renderer>().sharedMaterial = material;
 
 		material.SetFloat("_InnerRadius", innerRadius);
 		material.SetFloat("_OuterRadius", outerRadius);
 		material.SetColor("_Color", color);
-		material.SetColor("_Emission", emission);
 		material.SetFloat("_Smoothness", smoothness);
 		material.SetFloat("_RotationSpeed", rotationSpeed);
+		material.SetFloat("_NoiseScale", noiseScale);
+		material.SetFloat("_NoiseStrength", noiseStrength);
+	}
+
+	private void Start()
+	{
+		SetMaterialAttributes();
 	}
 
 	private void OnValidate()
