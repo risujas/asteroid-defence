9475018e6f5fbcaa5fbb4a59d0c20694dbad75e9
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-22 19:02:37 +0300

    Clean up random bits of code

--- Assets/Scripts/ConstantRotation.cs
+++ Assets/Scripts/ConstantRotation.cs
@@ -4,18 +4,6 @@ public class ConstantRotation : MonoBehaviour
 {
 	[SerializeField] protected Vector3 rotationSpeed;
 
-	public Vector3 RotationSpeed
-	{
-		set
-		{
-			rotationSpeed = value;
-		}
-		get
-		{
-			return rotationSpeed;
-		}
-	}
-
 	private void Update()
 	{
 		transform.Rotate(rotationSpeed * Time.smoothDeltaTime);
--- Assets/Scripts/FadeLight.cs
+++ Assets/Scripts/FadeLight.cs
@@ -3,25 +3,22 @@ using UnityEngine;
 
 public class FadeLight : MonoBehaviour
 {
+	[SerializeField] private Light fadeLight = null;
 	[SerializeField] private float fadeDuration = 0.5f;
 	[SerializeField] private float targetIntensity = 0.0f;
 
-	private Light fadeLight;
 	private float startIntensity;
 
 	private IEnumerator Fade()
 	{
-		fadeLight = GetComponent<Light>();
 		startIntensity = fadeLight.intensity;
 
 		float elapsed = 0.0f;
 		while (elapsed < fadeDuration)
 		{
 			elapsed += Time.deltaTime;
-
 			float t = Mathf.Clamp01(elapsed / fadeDuration);
 			fadeLight.intensity = Mathf.Lerp(startIntensity, targetIntensity, t);
-
 			yield return null;
 		}
 
@@ -30,6 +27,11 @@ public class FadeLight : MonoBehaviour
 
 	private void Start()
 	{
+		if (fadeLight == null)
+		{
+			fadeLight = GetComponent<Light>();
+		}
+
 		StartCoroutine(Fade());
 	}
 }
