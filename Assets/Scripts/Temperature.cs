bf31131d0fcda8a6db9d240790737a7c195dddf5
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-25 18:56:45 +0300

    Update Temperature.cs

--- Assets/Scripts/Temperature.cs
+++ Assets/Scripts/Temperature.cs
@@ -7,7 +7,7 @@ public class Temperature : MonoBehaviour
 	[SerializeField] private float maxEmissionIntensity = 10.0f;
 	[SerializeField] private float temperature = 0.0f;
 
-	private float temperatureDecay = 10.0f;
+	private float coolingSpeed = 50.0f;
 	private float minTemperature = 0.0f;
 	private float maxTemperature = 1000.0f;
 	private IntervalTimer adjustmentTimer = new IntervalTimer(1.0f / 30);
@@ -45,7 +45,7 @@ public class Temperature : MonoBehaviour
 	{
 		if (temperature > 0.0f)
 		{
-			temperature -= temperatureDecay * Time.deltaTime;
+			temperature -= coolingSpeed * Time.deltaTime;
 			temperature = Mathf.Clamp(temperature, minTemperature, maxTemperature);
 
 			if (adjustmentTimer.Tick())
