3eee63e53f8f5665f1e4c3b324a5bdcaedaeb1b5
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-26 16:30:53 +0300

    Refactor camera control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,7 +6,6 @@ public class CameraControl : MonoBehaviour
 
 	[SerializeField] private GameObject defaultFollowedObject;
 	[SerializeField] private GameObject followedObject;
-	[SerializeField] private float followSpeed = 2.0f;
 
 	[SerializeField] private float targetSize;
 	[SerializeField] private float minTargetSize = 1.0f;
@@ -14,6 +13,10 @@ public class CameraControl : MonoBehaviour
 
 	private float zoomSpeed = 3.0f;
 	private float zoomSmoothingSpeed = 6.0f;
+
+	private bool enableFollowObject = true;
+	private Vector3 followVelocity = Vector3.zero;
+	private float followSmoothTime = 0.25f;
 
 	private bool enableFollowReset = true;
 	private bool followReset = false;
@@ -34,6 +37,32 @@ public class CameraControl : MonoBehaviour
 		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetSize, zoomSmoothingSpeed * Time.unscaledDeltaTime);
 	}
 
+	private void HandleFollowing()
+	{
+		if (enableFollowObject)
+		{
+			if (followedObject != null)
+			{
+				var nextPosition = Vector3.SmoothDamp(transform.position, followedObject.transform.position, ref followVelocity, followSmoothTime);
+				nextPosition.z = cameraZ;
+				transform.position = nextPosition;
+			}
+			else if (enableFollowReset)
+			{
+				if (!followReset)
+				{
+					followReset = true;
+					followResetStart = Time.time;
+				}
+				else if (Time.time >= followResetStart + followResetTime)
+				{
+					followReset = false;
+					followedObject = defaultFollowedObject;
+				}
+			}
+		}
+	}
+
 	private void Start()
 	{
 		targetSize = Camera.main.orthographicSize;
@@ -44,29 +73,10 @@ public class CameraControl : MonoBehaviour
 		}
 	}
 
-	private void LateUpdate()
+	private void Update()
 	{
 		HandleZoom();
-
-		if (followedObject != null)
-		{
-			var nextPosition = Vector3.Lerp(transform.position, followedObject.transform.position, followSpeed * Time.deltaTime); ;
-			nextPosition.z = cameraZ;
-			transform.position = nextPosition;
-		}
-		else if (enableFollowReset)
-		{
-			if (!followReset)
-			{
-				followReset = true;
-				followResetStart = Time.time;
-			}
-			else if (Time.time >= followResetStart + followResetTime)
-			{
-				followReset = false;
-				followedObject = defaultFollowedObject;
-			}
-		}
+		HandleFollowing();
 	}
 
 	private void OnValidate()
