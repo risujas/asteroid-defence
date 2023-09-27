b6befe7687b5a39a1246daded58f535eb8a405ae
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-09-27 14:48:59 +0300

    PlanetClimate refactoring

--- Assets/Scripts/PlanetClimate.cs
+++ Assets/Scripts/PlanetClimate.cs
@@ -4,17 +4,34 @@ public class PlanetClimate : MonoBehaviour
 {
 	[SerializeField] private SunMotion sunMotion;
 
+	[SerializeField] private float iceCapCoverage = 0.0f;
+	[SerializeField] private float minCoverage = 0.1f;
+	[SerializeField] private float maxCoverage = 0.4f;
+
 	private Material material;
 
-	void Start()
+	private void SetIceCapCoverage(Material m)
+	{
+		iceCapCoverage = Mathf.Clamp(iceCapCoverage, minCoverage, maxCoverage);
+		m.SetFloat("_IceCapCoverage", iceCapCoverage);
+	}
+
+	private void Start()
 	{
 		material = GetComponent<Renderer>().material;
 	}
 
-	void Update()
+	private void Update()
 	{
 		float summerness = Mathf.Abs(0.5f - sunMotion.YearProgress) * 2.0f;
-		float icecapExtent = Mathf.Lerp(0.4f, 0.1f, summerness);
-		material.SetFloat("_IceCapCoverage", icecapExtent);
+
+		iceCapCoverage = Mathf.Lerp(maxCoverage, minCoverage, summerness);
+		SetIceCapCoverage(material);
+	}
+
+	private void OnValidate()
+	{
+		var m = GetComponent<Renderer>().sharedMaterial;
+		SetIceCapCoverage(m);
 	}
 }
