2b498f57204fa1eae64b68b05d8b5c5433a22614
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-10-10 13:48:25 +0300

    Make missiles "fire and forget" instead of manually controlled

--- Assets/Scripts/MissileControl.cs
+++ Assets/Scripts/MissileControl.cs
@@ -11,19 +11,7 @@ public class MissileControl : MonoBehaviour
 	[SerializeField] private VisualEffect rocketEngineVfx;
 
 	private Rigidbody rb;
-	private CameraControl cameraControl;
 	private FundsManager fundsManager;
-	private TimescaleChanger timescaleChanger;
-	private GravityBody gravityBody;
-
-	private void HandleRotation()
-	{
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		mousePos.z = transform.position.z;
-		Vector3 direction = mousePos - transform.position;
-
-		transform.up = direction.normalized;
-	}
 
 	private void HandleAcceleration()
 	{
@@ -32,30 +20,27 @@ public class MissileControl : MonoBehaviour
 			remainingDeltaV = maxDeltaV;
 		}
 
-		if (Input.GetMouseButton(0))
+		if (remainingDeltaV > 0.0f)
 		{
-			if (remainingDeltaV > 0.0f)
+			float deltaV = acceleration * Time.deltaTime;
+			if (remainingDeltaV < deltaV)
 			{
-				float deltaV = acceleration * Time.deltaTime;
-				if (remainingDeltaV < deltaV)
-				{
-					deltaV = remainingDeltaV;
-				}
+				deltaV = remainingDeltaV;
+			}
 
-				remainingDeltaV -= deltaV;
-				rb.AddForce(transform.up * deltaV, ForceMode.VelocityChange);
+			remainingDeltaV -= deltaV;
+			rb.AddForce(transform.up * deltaV, ForceMode.VelocityChange);
 
-				if (!rocketEngineSound.isPlaying)
-				{
-					rocketEngineSound.Play();
-					rocketEngineVfx.Play();
-				}
+			if (!rocketEngineSound.isPlaying)
+			{
+				rocketEngineSound.Play();
+				rocketEngineVfx.Play();
 			}
 		}
 
 		if (rocketEngineSound.isPlaying)
 		{
-			if (Input.GetMouseButtonUp(0) || remainingDeltaV <= 0.0f)
+			if (remainingDeltaV <= 0.0f)
 			{
 				rocketEngineSound.Stop();
 				rocketEngineVfx.Stop();
@@ -63,43 +48,15 @@ public class MissileControl : MonoBehaviour
 		}
 	}
 
-	private void OnEnable()
-	{
-		timescaleChanger = GameObject.FindWithTag("TimescaleChanger").GetComponent<TimescaleChanger>();
-		timescaleChanger.SetTimescale(1.0f);
-	}
-	private void OnDisable()
-	{
-		timescaleChanger.ResetTimescale();
-	}
-
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody>();
-
-		cameraControl = Camera.main.GetComponent<CameraControl>();
-		cameraControl.FollowedObject = gameObject;
-
 		fundsManager = GameObject.FindWithTag("FundsManager").GetComponent<FundsManager>();
-
-		gravityBody = GetComponent<GravityBody>();
 	}
 
 	private void Update()
 	{
-		HandleRotation();
 		HandleAcceleration();
-
-		if (Input.GetMouseButtonUp(1))
-		{
-			if (cameraControl.FollowedObject == gameObject)
-			{
-				cameraControl.FollowedObject = null;
-			}
-
-			gravityBody.UseCollisionSpeedThreshold = false;
-			enabled = false;
-		}
 	}
 
 	private void OnCollisionEnter(Collision collision)
