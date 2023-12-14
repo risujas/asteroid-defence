97939a60d9339a55d0064d41fd46bc0f91cfeb37
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-12-14 15:21:42 +0200

    Make laser cannon follow mouse

--- Assets/Scripts/LaserCannonControl.cs
+++ Assets/Scripts/LaserCannonControl.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserCannonControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = transform.position.z;
+		transform.up = (mousePos - transform.position).normalized;
+	}
 }
