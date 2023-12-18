0f6dad5e5b40c3a9ea4066731e51265ab59c6285
Johan Suorsa <56724011+risujas@users.noreply.github.com>
2023-12-18 10:01:22 +0200

    Add placeholder laser graphic

--- Assets/Scripts/LaserCannonControl.cs
+++ Assets/Scripts/LaserCannonControl.cs
@@ -2,10 +2,57 @@ using UnityEngine;
 
 public class LaserCannonControl : MonoBehaviour
 {
-	private void Update()
+	[SerializeField] private LayerMask laserCollisionLayers;
+	[SerializeField] private Transform laserOrigin;
+	private LineRenderer lineRenderer;
+
+	private void RotateTurret()
 	{
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		mousePos.z = transform.position.z;
 		transform.up = (mousePos - transform.position).normalized;
+	}
+
+	private void SetLinePositions()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = transform.position.z;
+
+		Ray ray = new Ray();
+		ray.origin = laserOrigin.position;
+		ray.direction = (mousePos - laserOrigin.position).normalized;
+
+		Vector3 laserEndPoint = laserOrigin.position;
+		laserEndPoint += ray.direction * 1000.0f;
+
+		if (Physics.Raycast(ray, out RaycastHit hit, 1000.0f, laserCollisionLayers))
+		{
+			laserEndPoint = hit.point;
+		}
+
+		lineRenderer.SetPosition(0, laserOrigin.position);
+		lineRenderer.SetPosition(1, laserEndPoint);
+	}
+
+	private void Awake()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+
+	private void OnEnable()
+	{
+		lineRenderer.enabled = true;
+		SetLinePositions();
+	}
+
+	private void OnDisable()
+	{
+		lineRenderer.enabled = false;
+	}
+
+	private void Update()
+	{
+		RotateTurret();
+		SetLinePositions();
 	}
 }
